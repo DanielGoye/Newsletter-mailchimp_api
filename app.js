843f9f2d352a94eaef2c310347f31e2e37a19bb2
Daniel Goye <danielgoye92@gmail.com>
2022-03-31 00:25:10 +0300

    djskj

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.POR || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running`);
 });
